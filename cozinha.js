29818b699057d7e371cccfdfce0451059a2b4fa1
MGSzabo <mgsbigboss.x1@gmail.com>
2026-07-09 14:20:07 -0300

    Update product links for air fryers

--- cozinha.js
+++ cozinha.js
@@ -2,13 +2,13 @@ const produtos = {
 
 "Air Fryers":[
 
-"Philips Walita Série 1000 XL 6,2L|https://.......",
+"Philips Walita Série 1000 XL 6,2L|https://s.shopee.com.br/W4uSs5FGp",
 
-"Midea Widemax 5,5L|https://.......",
+"Midea Widemax 5,5L|https://s.shopee.com.br/gOKeD3Pdm",
 
-"Mondial AFN-40-BI 4L 127V|https://.......",
+"Mondial AFN-40-BI 4L 127V|https://s.shopee.com.br/9Uzj9BWdE9",
 
-"Mondial AFN-40-BI 4L 220V|https://......."
+"Mondial AFN-40-BI 4L 220V|https://s.shopee.com.br/80AvMghyjE"
 
 ]
 
