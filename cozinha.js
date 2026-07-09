90fc162429a52aec7e5cba498c20f28a1b86615c
MGSzabo <mgsbigboss.x1@gmail.com>
2026-07-09 13:30:31 -0300

    Add airfryer data to cozinha.js

---
+++ cozinha.js
@@ -0,0 +1,27 @@
+const cozinha = {
+
+airfryers: [
+
+{
+nome: "Mondial AFN-40-BI 4L 127V",
+link: "https://..."
+},
+
+{
+nome: "Mondial AFN-40-BI 4L 220V",
+link: "https://..."
+},
+
+{
+nome: "Midea Widemax 5,5L",
+link: "https://..."
+},
+
+{
+nome: "Philips Walita Série 1000 XL 6,2L",
+link: "https://..."
+}
+
+]
+
+};
