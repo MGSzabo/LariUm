2fe0667744af1d8db7850744f3a53d795ffcc08b
MGSzabo <mgsbigboss.x1@gmail.com>
2026-07-09 16:32:36 -0300

    Add product categories and items to cozinha.js

--- cozinha.js
+++ cozinha.js
@@ -12,4 +12,56 @@ const produtos = {
 
 ]
 
+  const produtos = {
+
+"Panelas":[
+
+"Tramontina Turim 12 Peças Antiaderente com Utensílios|https://s.shopee.com.br/111CJ2f0lc",
+
+"Kit Jogo de Panelas Antiaderente 10 Peças com Tampa de Vidro e Utensílios|https://s.shopee.com.br/2qSqUbOr16",
+
+"Brinox Ceramic Life Smart Plus 5 Peças com Indução|https://s.shopee.com.br/1qaJJ24C8S"
+
+],
+
+"Cuscuzeiras":[
+
+"Cuscuzeira Tramontina Antiaderente 10cm 0,8L|https://s.shopee.com.br/4Vb4TYhKnm",
+
+"Cuscuzeira Tramontina Turim Antiaderente 10cm|https://s.shopee.com.br/1BKcVayiS7"
+
+],
+
+"Pipoqueiras":[
+
+"Pipoqueira Tramontina Loreto Antiaderente 20cm 3,5L|https://s.shopee.com.br/80Awe1gCBn"
+
+],
+
+"Frigideiras":[
+
+"Frigideira Rochedo Stone Pro Antiaderente 24cm|https://s.shopee.com.br/3ViXIOcuFs",
+
+"Frigideira Tramontina Turim Antiaderente Jogo 2 Peças|https://s.shopee.com.br/6ffZ3nSciM",
+
+"Frigideira Tramontina Turim Starflon Max 16, 20 e 24cm|https://s.shopee.com.br/20tjVZodGI"
+
+],
+
+"Cafeteiras":[
+
+"Cafeteira Elétrica Mondial Dolce Arome C-30 18 Xícaras|https://s.shopee.com.br/4AyE5sTyTa",
+
+"Cafeteira Espresso Oster Double Digital Cápsula e Pó|https://s.shopee.com.br/2Vq06tpgwx",
+
+"Cafeteira Elétrica Philco PCF20A 20 Xícaras Corta Pingos|https://s.shopee.com.br/9AMu3Csw7y"
+
+],
+
+"Leiteiras":[
+
+"Leiteira Fervedor Tramontina Turim Antiaderente 1,7L|https://s.shopee.com.br/1VxSuLP58C"
+
+]
+
 };
